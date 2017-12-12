8e6d5a9a9fcf14026a2e9e33de79f9a4090a32e3
MRJL1 <34366708+MRJL1@users.noreply.github.com>
2017-12-13 02:39:22 +0800

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,26 +1,31 @@
 import media
 import fresh_tomatoes
 
-iron_man = media.Movie("Iron Man",
-                     "a billionaire playboy philanthopist becomes a superhero by designing his own power suit",
-                     "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
-                      "https://www.youtube.com/watch?v=LWbjrFCecPY")
-
-batman_begins = media.Movie("Batman Begins",
-                     "a billionaire becomes a superhero by dressing up as bat",
-                     "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
-                      "https://www.youtube.com/watch?v=neY2xVmOfUM")
-
-wonder_woman = media.Movie("Wonder Woman",
-                     "A goddesss discovers that she is a goddess",
-                     "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
-                      "https://www.youtube.com/watch?v=VSB4wGIdDwo")
-
-thor = media.Movie("Thor",
-                     "A god lands on earth and kills his adapted brother",
-                     "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
-                      "https://www.youtube.com/watch?v=JOddp-nlNvQ")
-
+#ironman movie: movie title, storyline, poster image and movie trailer
+iron_man = media.Movie(
+    "Iron Man",
+    "a billionaire is a superhero",
+    "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+    "https://www.youtube.com/watch?v=LWbjrFCecPY")
+#batman begins movie: movie title, storyline, poster image and movie trailer
+batman_begins = media.Movie(
+    "Batman Begins",
+    "a billionaire is a superhero",
+    "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
+    "https://www.youtube.com/watch?v=neY2xVmOfUM")
+#wonder woman movie: movie title, storyline, poster image and movie trailer
+wonder_woman = media.Movie(
+    "Wonder Woman",
+    "A woman discovers that she is a goddess",
+    "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg"  # NOQA, 
+    "https://www.youtube.com/watch?v=VSB4wGIdDwo")
+#thor movie: movie title, storyline, poster image and movie trailer
+thor = media.Movie(
+    "Thor",
+    "A god kills his brother",
+    "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
+    "https://www.youtube.com/watch?v=JOddp-nlNvQ")
+#set the movies that will be passe to the media file
 movies = [iron_man, batman_begins, wonder_woman, thor]
+#open the HTML file in a webbrowser vis the fresh_totmates.py file
 fresh_tomatoes.open_movies_page(movies)
- 
--- media.py
+++ media.py
@@ -1,16 +1,15 @@
 import webbrowser
-
-class Movie():
+class Movie()
+    """ This class is made to store detailed information regarding movies."""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """initialises Movie class variables"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-   
 
     def show_trailer(self):
+        """ Opens Movie trailer in the web browser"""
         webbrowser.open(self.trailer_youtube_url)
-        
-    
